eb04981e9f147045585e04dac989f444948decb2
nayemislam33046 <nayemislam33046@gmail.com>
2024-11-12 11:07:44 +0600

    myCommit

--- App.js
+++ App.js
@@ -3,12 +3,7 @@ const cors = require("cors");
 const Product = require("./router/Products");
 const singleProduct = require("./router/SingleProduct");
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", Product);
--- Controller/Product.js
+++ Controller/Product.js
@@ -1,36 +1,5 @@
-const pool = require("../connection/db.js");
-const getProduct = (req, res) => {
-  try {
-    const sql = `SELECT p.id AS product_id, p.name, p.description, p.price, p.category, p.preprice,p.qty,pi.image_url FROM products p LEFT JOIN product_images pi ON p.id = pi.product_id ORDER BY p.id;`;
-    pool.query(sql, (err, data) => {
-      if (err) return res.json({ error: err.message });
-      const products = {};
-      data.forEach((row) => {
-        const productId = row.product_id;
-        if (!products[productId]) {
-          products[productId] = {
-            id: productId,
-            name: row.name,
-            description: row.description,
-            price: row.price,
-            category: row.category,
-            preprice: row.preprice,
-            images: [],
-            qty: row.qty,
-          };
-        }
-        if (
-          row.image_url &&
-          !products[productId].images.includes(row.image_url)
-        ) {
-          products[productId].images.push(row.image_url);
-        }
-      });
-      res.json(Object.values(products));
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to fetch products" });
-  }
-};
-module.exports = getProduct;
+const getProduct = (req,res)=>{
+  return res.send("This Is PRoduct")
+}
+
+module.exports = getProduct
